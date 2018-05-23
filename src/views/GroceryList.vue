a024cc4384a237ea87118f89d55166d8c6911fe9
Duco <duco@winterwerp.it>
2018-05-23 21:50:29 +0200

    Initial commit

---
+++ src/views/GroceryList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>Groceries</h1>
+    <input v-model="groceryInput" type="text" />
+    <button v-on:click="addGrocery">Add grocery</button>
+    <ul>
+        <li v-for="grocery in groceries">{{grocery}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+    name: 'groceryList',
+    data () {
+        return {
+            groceries: ["Ham", "Kaas", "Eieren"],
+            groceryInput: ""
+        }
+    },
+    methods: {
+        addGrocery () {
+            this.groceries.push(this.groceryInput);
+            this.groceryInput = "";
+        }
+    }
+}
+</script>
